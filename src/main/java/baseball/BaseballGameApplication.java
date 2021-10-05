4e3abb6284135c279e984a080f4a2bbdb938bb07
cwJohnPark <pierrotcw@gmail.com>
2021-10-05 19:33:26 +0900

    feat: 게임을 종료한 후 게임을 다시 시작하거나 종료할 수 있다.

--- src/main/java/baseball/BaseballGameApplication.java
+++ src/main/java/baseball/BaseballGameApplication.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import static baseball.code.ApplicationCode.*;
+
+import baseball.code.ApplicationCode;
 import baseball.controller.IBaseballGameController;
 import baseball.dto.HitResultDescription;
 import baseball.exception.InvalidUserInputException;
@@ -20,25 +23,53 @@ public class BaseballGameApplication {
 		int answer = AnswerGenerator.createThreeDigits();
 
 		while (continueGame) {
-			continueGame = hit(answer);
+			continueGame = hit(answer) != ANSWERED;
 		}
 		System.out.println("3 개의 숫자를 모두 맞히셨습니다! 게임 끝");
+
+		if (restart() == RESTART) {
+			start();
+		}
 	}
 
-	private boolean hit(int answer) {
+	private ApplicationCode hit(int answer) {
 		System.out.print("숫자를 입력해주세요: ");
 		try {
 			final HitResultDescription resultDescription = gameController.hit(toInteger(Console.readLine()), answer);
 			System.out.println(resultDescription.getDescription());
-			return !resultDescription.isEqualToAnswer();
+			return resultDescription.isEqualToAnswer() ? ANSWERED : CONTINUE;
 		} catch (InvalidUserInputException e) {
 			System.err.println(e.getMessage());
-			return true;
+			return CONTINUE;
 		}
 	}
 
-	private int toInteger(String inputLiteral) {
-		return Integer.parseInt(inputLiteral);
+	private ApplicationCode restart() {
+		boolean isInvalidInput = true;
+		int input = 0;
+		while (isInvalidInput) {
+			System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			input = getRestartInput();
+			isInvalidInput = input != 1 && input != 2;
+		}
+		return input == 1 ? RESTART : TERMINATE;
+	}
+
+	private int getRestartInput() {
+		try {
+			return toInteger(Console.readLine());
+		} catch (InvalidUserInputException e) {
+			System.err.println(e.getMessage());
+			return -1;
+		}
+	}
+
+	private int toInteger(String inputLiteral) throws InvalidUserInputException {
+		try {
+			return Integer.parseInt(inputLiteral);
+		} catch (NumberFormatException nfe) {
+			throw new InvalidUserInputException(inputLiteral);
+		}
 	}
 }
 
---
+++ src/main/java/baseball/code/ApplicationCode.java
@@ -0,0 +1,5 @@
+package baseball.code;
+
+public enum ApplicationCode {
+	RESTART, CONTINUE, ANSWERED, TERMINATE
+}
